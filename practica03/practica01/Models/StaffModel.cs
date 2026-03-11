d417c34dbfe7c725e985965fd000daeb69203a49
Putaku <kennethgranados64@gmail.com>
2026-03-10 20:41:18 -0600

    practica 03

---
+++ practica03/practica01/Models/StaffModel.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace practica01.Models
+{
+    public class StaffModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required]
+        public int StaffCategoryId { get; set; }
+
+        [ValidateNever]
+        public StaffCategoryModel StaffCategory { get; set; } = null!;
+
+        public int? SpecialtyId { get; set; }
+
+        [ValidateNever]
+        public SpecialtyModel? Specialty { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
